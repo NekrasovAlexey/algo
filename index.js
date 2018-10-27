8087edff6db3e5e74d973ebc4906019e561972b4
Nekrasov Alexey <avnekr@yandex.ru>
2018-10-27 15:07:43 +0300

    Улучшил сортировку пузырьком

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const bubleSort = array => {
     const sortArray = [...array];
 
-    for (let i = 0; i < sortArray.length; i++) {
-        for (let j = 0; j < sortArray.length - i - 1; j++) {
+    for (let i = sortArray.length - 1; i >= 0 ; i--) {
+        for (let j = 0; j < i; j++) {
             if (sortArray[j] > sortArray[j + 1]) {
                 const next = sortArray[j + 1];
                 sortArray[j + 1] = sortArray[j];
